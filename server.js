d48e0a92dcda2b2687c402904143e4ff6ff9626f
Yoo Park <74237804+yoondray@users.noreply.github.com>
2021-02-23 15:35:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -28,4 +28,33 @@ app.get('/home/about', function(req, res){
     res.render('about.html')
 })
 
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : '127.0.0.1',
+  user            : 'yoondray',
+  password        : '1234',
+  database        : 'study_db'
+});
+
+
+app.get('/db', function(req, res){
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT * FROM chatbot_train_data', function (error, results, fields) {
+          res.send(JSON.stringify(results));
+          console.log('results', results)
+            // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+})
+
 
